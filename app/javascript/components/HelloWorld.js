89e7ef0509e244ba506610f8dd71341db2dfb549
Leon Ji <leon@shoplineapp.com>
2019-08-23 03:26:51 -0800

    Have HelloWorld read React State and display “things”

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -13,10 +13,17 @@ function getThings() {
 
 class HelloWorld extends React.Component {
   render () {
+    const { things } = this.props;
+    const thingsList = things.map((thing, i) => {
+     return <li key={i}>{thing.name} {thing.guid}</li>
+    })
+
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
         <button className="getThingsBtn" onClick={() => this.props.getThings()}>getThings</button>
+        <br />
+        <ul>{ thingsList }</ul>
       </React.Fragment>
     );
   }
