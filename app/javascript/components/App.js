959702aa3175ca263eaf7a5110ba9edb48a3e423
Leon Ji <leon@shoplineapp.com>
2019-08-23 16:58:09 +0800

    Set up Redux State Store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,24 @@
 import React from "react"
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import HelloWorld from './HelloWorld'
+
+import configureStore from '../configureStore'
+const store = configureStore();
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => ("Home!")}/>
-          <Route path="/Hello" render={() => <HelloWorld greeting="Friend"/>} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() => ("Home!")} />
+            <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
